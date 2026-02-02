091045ecb84581c2b4a1ac0bc7797e60fdb058e6
kristiyanbstoychev <chrisbstoychev@gmail.com>
2026-02-02 12:25:26 +0200

    Added items view and db model

--- game/admin.py
+++ game/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 # This imports Character "blueprint" from models file
-from .models import Character
+from .models import Character, Item
 
 # This tells Django to show the Character section in the Admin dashboard
 admin.site.register(Character)
+admin.site.register(Item)
--- game/models.py
+++ game/models.py
@@ -14,3 +14,17 @@ class Character(models.Model):
     # This makes the character show their name in the admin panel
     def __str__(self):
         return self.name
+    
+# ===== ITEM MODEL =====
+# This represents equipment or loot
+class Item(models.Model):
+    name = models.CharField(max_length=100)
+    power = models.IntegerField(default=5)
+    
+    # This is the "Tether" (ForeignKey)
+    # It links the item to a specific Character
+    # 'on_delete=models.CASCADE' means if the hero is deleted, the item is too
+    owner = models.ForeignKey(Character, on_delete=models.CASCADE, related_name="items")
+
+    def __str__(self):
+        return f"{self.name} (Owned by {self.owner.name})"    
