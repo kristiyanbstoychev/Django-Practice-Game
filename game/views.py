1ae3abd103eeed1b2a722d1ec0c0758de1e5cbf9
kristiyanbstoychev <chrisbstoychev@gmail.com>
2026-02-02 11:58:32 +0200

    Added character view

--- game/views.py
+++ game/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+# This imports the Character model so the view can look at the data
+from .models import Character
 
-# Create your views here.
+# ===== CHARACTER LIST VIEW =====
+# This function handles the request to see all heroes
+def character_list(request):
+    # This asks the database: "Give me every character you have!"
+    all_characters = Character.objects.all()
+    
+    # We will start by creating a simple text string of names
+    # Like making a list of names on a piece of paper
+    names = ", ".join([c.name for c in all_characters])
+    
+    # This sends the list back to the user's browser
+    return HttpResponse(f"Heroes in this world: {names}")
